7b22761ba78379e446a40396bcaf36316f195eb6
Althea Hansel-Harris <ahansel@scripps.edu>
2023-05-15 11:23:21 -0700

    Add logistic parameter options

--- run_c2g.py
+++ run_c2g.py
@@ -6,11 +6,14 @@ from cryo2grid import Cryo2Grid
 parser = argparse.ArgumentParser(description='Convert CryoEM/X-ray maps to AutoDock grid maps.')
 parser.add_argument('-m', '--map-file', type=str, help='Map file name', required=True)
 parser.add_argument('-g', '--grid-maps', nargs='+', type=str, help='Grid map file name(s)', required=True)
+parser.add_argument('-l', '--log_min', type=float, help='Min for logistic curve', default=-3.0)
+parser.add_argument('-k', '--log_slope', type=float, help='slope for logistic curve', default=2.0)
+parser.add_argument('-x0', '--log_x0', type=float, help='Inflection point for logistic curve', default=0.5)
 args = parser.parse_args()
 
 c2g = Cryo2Grid(args.map_file)
 grid_maps = c2g.ReadGridMaps(args.grid_maps)
 density   = c2g.ReadMapFile()
-modified  = c2g.ModifyDensity(density)
+modified  = c2g.ModifyDensity(density, args.log_min, args.log_slope, args.log_x0)
 c2g.WriteGridMaps(modified, 1)
 
